39063b65271c7312e8f567b7f140da85ac481736
johnpori <h.kawaguchi0610@gmail.com>
2022-01-29 21:45:12 +0900

    D-2完成(rectangle1,rectangle2)

--- D-2.py
+++ D-2.py
@@ -17,9 +17,16 @@ class Rectangle:
 
 # 実体化(インスタンス化)
 rectangle1 = Rectangle(height=5, width=6)
+rectangle2 = Rectangle(height=3, width=3)
 
-# 結果-長方形面積
+# rectangle1結果-長方形面積
 print(rectangle1.area())  # 30.00
 
-# 結果-対角線の長さ
+# rectangle1結果-対角線の長さ
 print(rectangle1.diagonal())  # 7.81
+
+# rectangle2結果-長方形面積
+print(rectangle2.area())  # 9.00
+
+# rectangle2結果-対角線の長さ
+print(rectangle2.diagonal())  # 4.24
