9c5ea259397db753bb173c5b88f89b50308cfa2a
johnpori <h.kawaguchi0610@gmail.com>
2022-01-29 21:47:01 +0900

    D-1完成-若干修正

--- D-1.py
+++ D-1.py
@@ -14,10 +14,14 @@ class Circle:
         return f'{self.radius * 2 * math.pi:.2f}'
 
 
+# 実体化(インスタンス化)
 circle1 = Circle(radius=1)
+circle3 = Circle(radius=3)
+
+# circle1結果
 print(circle1.area())  # 3.14
 print(circle1.perimeter())  # 6.28
 
-circle3 = Circle(radius=3)
+# circle3結果
 print(circle3.area())  # 28.27
 print(circle3.perimeter())  # 18.85
