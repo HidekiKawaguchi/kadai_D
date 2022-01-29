4352ce1926991f8758ec4787c6b1b85e8489f0d7
johnpori <h.kawaguchi0610@gmail.com>
2022-01-30 06:36:55 +0900

    D-4修正

--- D-4.py
+++ D-4.py
@@ -19,3 +19,12 @@ print(counter1.value)  # 1
 
 counter1.count_up()
 print(counter1.value)  # 2
+
+counter2 = MyCounterV1(value=7)
+print(counter2.value)  # 7
+
+counter2.count_up()
+print(counter2.value)  # 8
+
+counter2.count_up()
+print(counter2.value)  # 9
